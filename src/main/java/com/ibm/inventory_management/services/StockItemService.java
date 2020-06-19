ac98f1f48124120603414ad7a25913463043460a
Ramya Raghuveera <ramyaraghuveera@Ramyas-MacBook-Pro.local>
2020-06-19 20:31:19 +0530

    Added primary package

--- src/main/java/com/ibm/inventory_management/services/StockItemService.java
+++ src/main/java/com/ibm/inventory_management/services/StockItemService.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 
 import com.cloudant.client.api.CloudantClient;
 import com.cloudant.client.api.Database;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
